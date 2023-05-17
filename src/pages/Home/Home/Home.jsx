6b378ed441ddf31b20390decb35948f74036b461
Mahfuz The Programmer Vetern <mahfuzzayn8@gmail.com>
2023-05-17 22:21:51 +0600

    Added Navigation bar using Daisyui Components

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "../../Shared/NavBar/NavBar";
+
+const Home = () => {
+    return (
+        <div className="home">
+            <NavBar></NavBar>
+        </div>
+    );
+};
+
+export default Home;
